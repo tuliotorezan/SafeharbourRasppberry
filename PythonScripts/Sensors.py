4b36c96abad85109ab43e3df0febe2cacfcfb694
tuliotorezan <tuliotorezan@gmail.com>
2023-05-18 10:47:20 -0300

    adding python script that gets data from raspi sensors

---
+++ PythonScripts/Sensors.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from sense_hat import SenseHat
+import json
+
+sense = SenseHat()
+
+sense.set_imu_config(True, True, True)
+
+
+
+orientation = sense.get_orientation_degrees()
+humidity = sense.get_humidity()
+temp = sense.get_temperature()
+pressure = sense.get_pressure()
+north = sense.get_compass()
+gyro_only = sense.get_gyroscope()
+accel_only = sense.get_accelerometer()
+
+json_data ={
+    'orientation': orientation,
+    'humidity': humidity,
+    'temp': temp,
+    'pressure': pressure,
+    'north': north,
+    'gyro_only': gyro_only,
+    'accel_only': accel_only,    
+    }
+
+with open("sensor_data.json", "w", encoding='utf-8') as fp:
+    json.dump(json_data, fp, sort_keys=True, indent=4)
+
+#sense.show_message("SAFE HARBOUR TECH!")
+#sense.show_message("Temperature: %.2f C" % temp, text_colour=[255,0,0])
